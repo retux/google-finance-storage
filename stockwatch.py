b1458dc515c7b0906fbc5696b89f32068510f1f9
Matias Gutierrez Reto <mati.gutierrez@globant.com>
2017-07-25 19:19:41 +0100

    isolated client from class

--- stockwatch.py
+++ stockwatch.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 """
 stock-watch: gets stock quotes from Google Financial API and stores in a db.
@@ -9,6 +8,8 @@ stock-watch: gets stock quotes from Google Financial API and stores in a db.
 	Each element should be 'EXCHANGE:SYMBOL' examples
 
 	[ 'NASDAQ:GOOG', 'NASDAQ:CSCO', 'NYSE:IBM', 'BCBA:YPFD' ]
+	
+	Author: retux (twitter: @retux)
 
 """
 
@@ -186,42 +187,6 @@ class GoogleFinanceAPI:
 		finally:
 			db.close()
 
-
-
-
-def main():
-
-	"""
-	Symbols list contain a list of pairs which describes stock symbols as used by Google API.
-	Each element should be 'EXCHANGE:SYMBOL' examples:
-	
-		 [ 'NASDAQ:GOOG', 'NASDAQ:CSCO', 'NYSE:IBM', 'BCBA:YPFD' ]
-	"""
-
-	Symbols = [ 'NASDAQ:GOOG', 'NASDAQ:CSCO', 'NASDAQ:BABA', 'NASDAQ:APPL', 'NYSE:IBM', 'BCBA:YPFD' , 'NYSE:GLOB' ]
-	#Symbols = [ 'NASDAQ:GOOG' ]
-
-	strSymbols = ' ' 
-	o = 0
-	for i in Symbols:
-		if o == 0:
-			strSymbols = i
-		else:
-			strSymbols = strSymbols + ',' + i
-		o += 1
-
-	JSp = GoogleFinanceAPI()
-
-	#if JSp.getJsonFromFile():	# // gets data from test file, just for debbugging.
-	if JSp.get(strSymbols):
-		#JSp.Quotes2Stdout()	# // Show a little data, just for testing
-		JSp.JsonQot2Obj()
-		JSp.DumpSnap2Sqlite()
-		JSp.UpdateDaySqlite()
-
-
-if __name__ == "__main__":
-	main()
 
 
 
